fd2ef42eeff14734415e00a41b6bf414e49ff1a9
yaoxingyu <yaoxingyu@pwrd.com>
2019-04-02 19:42:51 +0800

    爬楼梯（递归） 冒泡排序

--- DataStructureAndAlgorithms/DataStructureAndAlgorithms/BubbleSort.h
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithms/BubbleSort.h
@@ -6,6 +6,7 @@ void BubbleSort(int a[], int n)
 	int tmp = 0;
 	for (int i = 0; i < n-1; i++)
 	{
+		bool bFlag = false;
 		for (int j = 0; j < n-1-i; j++)
 		{
 			if (a[j]>a[j+1])
@@ -13,7 +14,12 @@ void BubbleSort(int a[], int n)
 				tmp = a[j];
 				a[j] = a[j + 1];
 				a[j + 1] = tmp;
+				bFlag = true;
 			}
+		}
+		if (!bFlag)
+		{
+			break;
 		}
 	}
 }
