ade3a7d7c980e90ac7408826d81466033d67151c
yaoxingyu <yaoxingyu@pwrd.com>
2019-05-23 17:31:59 +0800

    归并排序：

---
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithms/MergeSort.h
@@ -0,0 +1,88 @@
+#pragma once
+//#include <vector>
+
+void Merge(int a[], int tmp[], int low, int mid, int high)
+{
+	int i = low, j = mid + 1, pos = low;
+	while (i<=mid && j<=high)
+	{
+		if (a[i] < a[j])
+		{
+			tmp[pos++] = a[i++];
+		}
+		else
+		{
+			tmp[pos++] = a[j++];
+		}
+	}
+
+	while (i <= mid)
+	{
+		tmp[pos++] = a[i++];
+	}
+
+	while (j <= high)
+	{
+		tmp[pos++] = a[j++];
+	}
+
+	for (int i = low; i <= high; i++)
+	{
+		a[i] = tmp[i];
+	}
+}
+
+void MergeSort(int a[], int tmp[], int low, int high)
+{
+	if (low >= high)
+		return;
+
+	int mid = low + (high - low) / 2;
+	MergeSort(a, tmp, low, mid);
+	MergeSort(a, tmp, mid+1, high);
+	Merge(a, tmp, low, mid, high);
+}
+
+
+/*std::vector<int> Merge(std::vector<int> vec1, std::vector<int> vec2)
+{
+	std::vector<int> result;
+	int i = 0, j = 0;
+	while (i < vec1.size() && j < vec2.size())
+	{
+		if (vec1[i] < vec2[j])
+		{
+			result.push_back(vec1[i]);
+			i++;
+		}
+		else
+		{
+			result.push_back(vec2[j]);
+			j++;
+		}
+	}
+
+	if (i < vec1.size())
+	{
+		result.insert(result.end(), vec1.begin() + i, vec1.end());
+	}
+	else
+	{
+		result.insert(result.end(), vec2.begin() + j, vec2.end());
+	}
+
+	return result;
+}
+
+std::vector<int> MergeSort(std::vector<int> vec)
+{
+	if (vec.size() <= 1)
+	{
+		return vec;
+	}
+	else
+	{
+		return Merge(MergeSort(std::vector<int>(vec.begin(), vec.begin() + vec.size() / 2)), 
+			         MergeSort(std::vector<int>(vec.begin() + vec.size()/2, vec.end())));
+	}
+}*/
--- DataStructureAndAlgorithms/DataStructureAndAlgorithms/main.cpp
+++ DataStructureAndAlgorithms/DataStructureAndAlgorithms/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <vector>
+
 #include "LadderStep.h"
 #include "BubbleSort.h"
 #include "InsertSort.h"
 #include "SelectSort.h"
+#include "MergeSort.h"
 
 using namespace std;
 
 int main()
 {
 	// √∞≈›≈≈–Ú
-	int a[] = {1, 43, 2, 89, 565, 66, 4, 59, 0};
+	int a[] = {9,8,7,6,5,4,3,2,1};
 	//BubbleSort(a, sizeof(a)/4);
-	InsertSort(a, sizeof(a) / 4);
+	//InsertSort(a, sizeof(a) / 4);
 	//SelectSort(a, sizeof(a) / 4);
+	const int length = sizeof(a) / sizeof(int);
+	int tmp[length];
+	MergeSort(a, tmp, 0, length-1);
 
 	// ≈¿¬•Ã›Œ Ã‚
 	//cout << LadderStep::ClimbLadder(7) << endl;
